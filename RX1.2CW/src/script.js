dfe012a2dfc3bb1bc4f73aa92e809fb256ffcd0b
VIVEK SHUKLA <primevivek14@gmail.com>
2026-03-01 08:03:22 +0530

    done lession

--- RX1.2CW/src/script.js
+++ RX1.2CW/src/script.js
@@ -11,7 +11,7 @@ const todoList = document.querySelector("#todoList")
 
 
 
- const store = createStore(todoReducer)
+ const store = createStore(todoReducer , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
  store.subscribe(()=>{
     console.log(store.getState())
     updateTodoList()
