bd183358278bb3cad80ead9d5bbccc173d24d7b8
VIVEK SHUKLA <primevivek14@gmail.com>
2026-03-11 15:48:03 +0530

    done somthing

--- RX2.1_CW/src/App.jsx
+++ RX2.1_CW/src/App.jsx
@@ -1,5 +1,4 @@
 
-import './App.css'
 import {useDispatch , useSelector } from "react-redux"
 
 
--- RX2.1_CW/src/main.jsx
+++ RX2.1_CW/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import store from './store.js'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
